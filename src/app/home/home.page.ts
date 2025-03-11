8bba5a922eabea0155224b1bb0b7fd3193638191
Andycb000 <19030@portalcedi.edu.mx>
2025-03-11 10:28:27 -0600

    EA17 -> Esqueleto terminado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel],
 })
 export class HomePage {
-  constructor() {}
+  constructor() { }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDeeKvtjIu1pidZ4oJTmSe1TvMHoXiHkII",
+    authDomain: "creacion-de-crud-b5d99.firebaseapp.com",
+    projectId: "creacion-de-crud-b5d99",
+    storageBucket: "creacion-de-crud-b5d99.firebasestorage.app",
+    messagingSenderId: "786160780710",
+    appId: "1:786160780710:web:722757a9d24eb0327fbb67"
+  }
 };
 
 /*
